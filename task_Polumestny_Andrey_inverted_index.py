b77b93cd5497ae3ed5952f0e7f7212031e93eae2
Andrey <anpolumestny@gmail.com>
2022-11-29 22:17:11 +0300

    lesson 3

---
+++ task_Polumestny_Andrey_inverted_index.py
@@ -0,0 +1,257 @@
+"""
+module provide utils for work with inverted index
+"""
+from __future__ import annotations
+
+import argparse
+import json
+import re
+import sys
+from pathlib import Path
+from struct import calcsize, pack, unpack
+from typing import Dict, List
+
+BASE_DIR = Path(__file__).parent.resolve()
+
+
+class InvertedIndex:
+    """class represented inverted index"""
+
+    def __init__(self, inverted_index: Dict[str, list[str]]):
+        self.index = inverted_index
+
+    def __eq__(self, other: InvertedIndex):
+        return self.index == other.index
+
+    def __str__(self):
+        return str(self.index)
+
+    def query(self, words: List[str]) -> List[int]:
+        """Return the list of relevant documents for the given query"""
+        # print(self.index, file=sys.stderr)
+        # print(words, file=sys.stderr)
+        relevant_documents = []
+        for word in words:
+            # print(word, file=sys.stderr)
+            documents = self.index.get(word, None)
+            if not documents:
+                return []
+            tmp_relevant_documents = [int(document) for document in documents]
+            if not relevant_documents:
+                relevant_documents = tmp_relevant_documents[:]
+            else:
+                relevant_documents = list(
+                    set(relevant_documents).intersection(
+                        tmp_relevant_documents))
+        return relevant_documents
+
+    def dump(self, filepath: str, method='struct') -> None:
+        """save inverted index in file"""
+        file = Path(filepath)
+        if not file.is_absolute():
+            file = BASE_DIR.joinpath(file)
+        if method == 'json':
+            self.json_dump(file)
+        else:
+            self.struct_dump(file)
+
+    def json_dump(self, file: str) -> None:
+        """save inverted index in file with json algorithm"""
+        with open(file, 'w') as f_out:
+            json.dump(self.index, f_out)
+
+    def struct_dump(self, file: str) -> None:
+        """save inverted index if file with struct algorithm"""
+        pairs = {}
+        documents_id = []
+        with open(file, 'wb') as f_out:
+            for word, documents in self.index.items():
+                pairs[word] = len(documents)
+                for doc_id in documents:
+                    documents_id.append(doc_id)
+            header = json.dumps(pairs).encode('utf8')
+            meta = len(header)
+            f_out.write(pack('I', meta))
+            f_out.write(pack(f'{meta}s', header))
+            for doc_id in documents_id:
+                f_out.write(pack('H', doc_id))
+
+    @classmethod
+    def load(cls, filepath: str, strategy='struct') -> InvertedIndex:
+        """load inverted index from file"""
+        file = Path(filepath)
+        if not file.is_absolute():
+            file = BASE_DIR.joinpath(file)
+        if strategy == 'json':
+            return cls.load_from_json(file)
+        else:
+            return cls.load_from_binary(file)
+
+    @classmethod
+    def load_from_json(cls, file):
+        with open(file, 'r') as f_in:
+            inverted_index = json.load(f_in)
+            # print(inverted_index, file=sys.stderr)
+        inverted_index_instance = InvertedIndex(inverted_index)
+        return inverted_index_instance
+
+    @classmethod
+    def load_from_binary(cls, file):
+        # print('start loading', file=sys.stderr)
+        with open(file, 'rb') as f_in:
+            data = f_in.read()
+            unsigned_index = calcsize('I')
+            meta, = unpack('I', data[:unsigned_index])
+            header, = unpack(f'{meta}s', data[unsigned_index:unsigned_index + meta])
+            header_json = json.loads(header.decode('utf8'))
+            inverted_index = {}
+            current_index = unsigned_index + meta
+            unsigned_short = calcsize('H')
+            for word, doc_count in header_json.items():
+                documents = []
+                for count in range(1, doc_count + 1):
+                    doc_id, = unpack('H', data[current_index: current_index + unsigned_short])
+                    documents.append(doc_id)
+                    current_index += unsigned_short
+                inverted_index[word] = documents
+            inverted_index_instance = InvertedIndex(inverted_index)
+            return inverted_index_instance
+
+
+def load_documents(filepath: str) -> Dict[int, str]:
+    """read file and load it to memory"""
+    file = Path(filepath)
+    if not file.is_absolute():
+        file = BASE_DIR.joinpath(file)
+
+    if not file.exists():
+        raise FileNotFoundError
+    in_memory_doc = {}
+    with open(file, 'r') as f_in:
+        for line in f_in:
+            doc_id, content = line.lower().split("\t", 1)
+            doc_id = int(doc_id)
+            in_memory_doc[doc_id] = content.strip()
+    return in_memory_doc
+
+
+def build_inverted_index(documents: Dict[int, str]) -> InvertedIndex:
+    """build inverted index from a document"""
+    inv_index = {}
+    for key, value in documents.items():
+        for word in re.split(r"\W+", value):
+            if word not in inv_index:
+                inv_index[word] = [key]
+            elif word in inv_index and key not in inv_index[word]:
+                inv_index[word].append(key)
+    inv_index_instance = InvertedIndex(inv_index)
+    return inv_index_instance
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description='Inverted Index CLI',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    subparser = parser.add_subparsers(help='list of allowed commands')
+
+    build_parser = subparser.add_parser(
+        'build',
+        help='build inverted index',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    build_parser.add_argument(
+        '--strategy', choices=('json', 'struct'),
+        default='struct',
+        help='strategy to store inverted index'
+    )
+    build_parser.add_argument(
+        '--dataset',
+        required=True,
+        help='path to dataset'
+    )
+    build_parser.add_argument(
+        '--output',
+        required=True,
+        help='path to inverted index dump'
+    )
+    build_parser.set_defaults(callback=build_callback)
+
+    query_parser = subparser.add_parser(
+        'query',
+        help='retrieve documents from inverted index',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    query_file_group = query_parser.add_mutually_exclusive_group(required=True)
+    query_file_group.add_argument('--query-file-utf8',
+                                  dest='query_file',
+                                  type=argparse.FileType('r',
+                                                         encoding='utf-8'))
+    query_file_group.add_argument('--query-file-cp1251',
+                                  dest='query_file',
+                                  type=argparse.FileType('r',
+                                                         encoding='cp1251'))
+    query_file_group.add_argument('--query',
+                                  dest='query',
+                                  nargs='*',
+                                  action='append')
+    query_parser.add_argument(
+        '--index',
+        dest='inverted_index_path',
+    )
+    query_parser.add_argument('--strategy',
+                              choices=('json', 'struct'),
+                              default='struct')
+    query_parser.set_defaults(callback=query_callback)
+
+    return parser.parse_args()
+
+
+def build_callback(arguments):
+    build(arguments.dataset, arguments.output, arguments.strategy)
+
+
+def build(dataset_filepath, output_filepath, strategy):
+    documents = load_documents(dataset_filepath)
+    inverted_index_instance = build_inverted_index(documents)
+    inverted_index_instance.dump(output_filepath, strategy)
+    # print(f'build completed in {output_filepath}', file=sys.stderr)
+
+
+def query_callback(arguments):
+    # print(arguments, file=sys.stderr)
+    if arguments.query_file:
+        query_from_file(arguments.inverted_index_path, arguments.query_file)
+    else:
+        query_from_list(arguments.inverted_index_path, arguments.query)
+    # print('query completed successfully', file=sys.stderr)
+
+
+def query_from_file(inverted_index_path, query_file):
+    inverted_index = InvertedIndex.load(inverted_index_path)
+    for words in query_file:
+        # print(words, file=sys.stderr)
+        documents = inverted_index.query(words.strip().split(' '))
+        if documents:
+            print(','.join([str(doc_id) for doc_id in documents]))
+        else:
+            print()
+
+
+def query_from_list(inverted_index_path, query):
+    inverted_index = InvertedIndex.load(inverted_index_path)
+    for words in query:
+        documents = inverted_index.query(words)
+        if documents:
+            print(','.join([str(doc_id) for doc_id in documents]))
+        else:
+            print()
+
+
+def main(arg=None):
+    args = cli()
+    args.callback(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_Polumestny_Andrey_inverted_index.py
@@ -0,0 +1,140 @@
+"""
+test for inverted_index module
+"""
+from textwrap import dedent
+
+import pytest
+
+
+import task_Polumestny_Andrey_inverted_index as inverted_index
+
+
+@pytest.fixture(ids='small_dataset')
+def small_doc(tmpdir):
+    """
+    fixture load in memory small document
+    """
+    data = dedent("""\
+    12	Anarchism         Anarchism is often defined as topic test
+    25	Test             Another topic 
+    """)
+    small_dataset = tmpdir.join('small_dataset')
+    small_dataset.write(data)
+    documents = inverted_index.load_documents(small_dataset)
+    return documents
+
+
+@pytest.fixture(ids='small_inverted_index')
+def small_inverted_index(small_doc):
+    """
+    build inverted index base on small_doc fixture
+    """
+    small_inverted_index = inverted_index.build_inverted_index(small_doc)
+    return small_inverted_index
+
+
+def test_load_non_existing_documents():
+    """
+    test raising exception when load not existing document
+    """
+    with pytest.raises(FileNotFoundError):
+        inverted_index.load_documents('datasets/some_file.txt')
+
+
+def test_load_doc():
+    """
+    test load_document function
+    """
+    file_dump = inverted_index.load_documents('datasets/small_dataset')
+    err_msg = (f'loaded file should be a dict instance'
+               f' instead of {type(file_dump)}')
+    assert isinstance(file_dump, dict), err_msg
+
+
+def test_build_inverted_index_return(small_doc):
+    inv_index = inverted_index.build_inverted_index(small_doc)
+    err_msg = (f'build_inverted_index should return InvertedIndex,'
+               f' instead of {type(inv_index)}')
+    assert isinstance(inv_index, inverted_index.InvertedIndex), err_msg
+
+
+def test_build_inverted_index_for_same_doc_equal(small_doc):
+    """
+    test __eq__ method of InvertedIndex
+    """
+    inv_index_1 = inverted_index.build_inverted_index(small_doc)
+    inv_index_2 = inverted_index.build_inverted_index(small_doc)
+    err_msg = 'same documents return different inverted index'
+    assert inv_index_1 == inv_index_2, err_msg
+
+
+def test_load_doc_count_of_topic():
+    """
+    test cont of topic in inmemory doc
+    """
+    expected_count = 3
+    file_dump = inverted_index.load_documents('datasets/small_dataset')
+    topic_count = len(file_dump)
+    err_msg = f'topic count is {topic_count} instead of {expected_count}'
+    assert expected_count == topic_count, err_msg
+
+
+@pytest.mark.parametrize(
+    'words, answer',
+    [
+        pytest.param(['anarchism'], [12], id='one_document_matching'),
+        pytest.param(['topic'], [12, 25], id='two_document_matching'),
+        pytest.param(['topic', 'test'],
+                     [25, 12],
+                     id='list_of_words_in_two_document_matching'),
+        pytest.param(['topic', 'something'],
+                     [],
+                     id='list_of_words_in_two_document_not_found'),
+        pytest.param(['None'], [], id='not_found_document'),
+    ]
+)
+def test_query_inverted_index(small_inverted_index, words, answer):
+    """
+    test InvertedIndex query method
+    """
+    documents = small_inverted_index.query(words)
+    err_msg = f'wrong documents found, expected {answer}, got {documents}'
+    assert documents == answer, err_msg
+
+
+def test_dump_inverted_index(small_inverted_index, tmpdir):
+    """
+    test InvertedIndex dump method
+    """
+    file = tmpdir.join('inverted_index_dump.json')
+    small_inverted_index.dump(file)
+
+    assert file.exists(), 'file did not create'
+    assert file.size() > 0, 'empty file created'
+
+
+def test_inverted_index_load(small_inverted_index):
+    """
+    test InvertedIndex load method
+    """
+    document = inverted_index.load_documents('datasets/small_dataset')
+    inv_index = inverted_index.build_inverted_index(document)
+    inv_index.dump('datasets/small_dataset.json')
+    loaded_inv_index = inv_index.load('datasets/small_dataset.json')
+
+    err_msg = 'loaded inverted index not equal with expected'
+    assert inv_index == loaded_inv_index, err_msg
+
+
+# @pytest.mark.parametrize()
+def test_cli_query_from_file(capsys):
+    query_file = 'datasets/query_utf8.txt'
+    with open(query_file, 'r') as query_file_in:
+        inverted_index.query_from_file(inverted_index_path='datasets/inv_index_struct_test', query_file=query_file_in)
+        captured = capsys.readouterr()
+        assert '12,25' in captured.out
+        assert 'topic' in captured.err
+        assert '12,25' not in captured.err
+        assert 'topic' not in captured.out
+
+
